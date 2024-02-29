399d035fd2899612a8f769b3b968e8f387f2b98a
nidaonder <nidaaonder@gmail.com>
2024-02-29 14:23:43 +0300

    Added getPrice methods.

--- src/SecondWeek/homework1/Main.java
+++ src/SecondWeek/homework1/Main.java
@@ -9,25 +9,25 @@ public class Main {
 
         HousingInventory inventory = new HousingInventory();
 
-        House house1 = new House(300000D, 100, 2,1);
-        House house2 = new House(400000D,120, 3,1 );
-        House house3 = new House(500000D, 140, 3, 1);
+        House house1 = new House(300000L, 100, 2,1);
+        House house2 = new House(400000L,120, 3,1 );
+        House house3 = new House(500000L, 140, 3, 1);
 
         inventory.addHouse(house1);
         inventory.addHouse(house2);
         inventory.addHouse(house3);
 
-        Villa villa1 = new Villa(700000D, 150, 3, 1);
-        Villa villa2 = new Villa(900000D, 170, 4, 1);
-        Villa villa3 = new Villa(1200000D, 210, 5,2);
+        Villa villa1 = new Villa(700000L, 150, 3, 1);
+        Villa villa2 = new Villa(900000L, 170, 4, 1);
+        Villa villa3 = new Villa(1200000L, 210, 5,2);
 
         inventory.addVilla(villa1);
         inventory.addVilla(villa2);
         inventory.addVilla(villa3);
 
-        SummerHouse summerHouse1 = new SummerHouse(2500000D, 210, 5,2);
-        SummerHouse summerHouse2 = new SummerHouse(2700000D, 220, 5,2);
-        SummerHouse summerHouse3 = new SummerHouse(2100000D, 190, 4,1);
+        SummerHouse summerHouse1 = new SummerHouse(2500000L, 210, 5,2);
+        SummerHouse summerHouse2 = new SummerHouse(2700000L, 220, 5,2);
+        SummerHouse summerHouse3 = new SummerHouse(2100000L, 190, 4,1);
 
         inventory.addSummerHouse(summerHouse1);
         inventory.addSummerHouse(summerHouse2);
@@ -38,5 +38,10 @@ public class Main {
         service.printHouses();
         service.printVillas();
         service.printSummerHouse();
+
+        System.out.println("Total price of houses = " + service.getTotalPriceOfHouses());
+        System.out.println("Total price of villas = " + service.getTotalPriceOfVillas());
+        System.out.println("Total price of summer houses = " + service.getTotalPriceOfSummerHouses());
+        System.out.println("Total price of all housings = " + service.getTotalPriceOfAllHousing());
     }
 }
