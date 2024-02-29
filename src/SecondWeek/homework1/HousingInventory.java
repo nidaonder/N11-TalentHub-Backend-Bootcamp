c33c9ce46830ee5668d59877ad8b9c933cc66c2d
nidaonder <nidaaonder@gmail.com>
2024-02-29 12:52:46 +0300

    Added HousingInventory.

--- src/SecondWeek/homework1/HousingInventory.java
+++ src/SecondWeek/homework1/HousingInventory.java
@@ -1,4 +1,42 @@
 package SecondWeek.homework1;
 
+import SecondWeek.homework1.entity.House;
+import SecondWeek.homework1.entity.SummerHouse;
+import SecondWeek.homework1.entity.Villa;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HousingInventory {
+    private final List<House> houseList = new ArrayList<>();
+    private final List<Villa> villaList = new ArrayList<>();
+    private final List<SummerHouse> summerHouseList = new ArrayList<>();
+
+    public HousingInventory() {
+    }
+
+    public List<House> getHouseList() {
+        return this.houseList;
+    }
+
+    public List<Villa> getVillaList() {
+        return this.villaList;
+    }
+
+    public List<SummerHouse> getSummerHouseList() {
+        return this.summerHouseList;
+    }
+
+    public void addHouse(House house) {
+        this.houseList.add(house);
+    }
+
+    public void addVilla(Villa villa) {
+        this.villaList.add(villa);
+    }
+
+    public void addSummerHouse(SummerHouse summerHouse) {
+        this.summerHouseList.add(summerHouse);
+    }
+
 }
