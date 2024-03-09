f56a16db03b42a1fe9c5464d40978b15acbddf78
nidaonder <nidaaonder@gmail.com>
2024-03-09 21:32:15 +0300

    added weather api project.

---
+++ FourthWeek/weatherapi/src/main/java/com/nidaonder/weatherapi/controller/WeatherController.java
@@ -0,0 +1,34 @@
+package com.nidaonder.weatherapi.controller;
+
+import com.nidaonder.weatherapi.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/weather")
+@Slf4j
+@RequiredArgsConstructor
+public class WeatherController {
+
+    //TODO disable H2 db@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    private final WeatherService weatherService;
+
+    @GetMapping("/daily/{city}")
+    public ResponseEntity<JSONObject> getDailyWeather(@PathVariable String city){
+        return weatherService.getDailyWeather(city);
+    }
+
+    @GetMapping("/weekly/{city}")
+    public ResponseEntity<String> getWeeklyWeather(@PathVariable String city){
+        return weatherService.getWeeklyWeather(city);
+    }
+
+    @GetMapping("/monthly/{city}")
+    public ResponseEntity<String> getMonthlyWeather(@PathVariable String city){
+        return weatherService.getMonthlyWeather(city);
+    }
+
+}
---
+++ FourthWeek/weatherapi/src/main/java/com/nidaonder/weatherapi/service/WeatherService.java
@@ -0,0 +1,49 @@
+package com.nidaonder.weatherapi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class WeatherService {
+
+    @Value("${weather-api.api.key}")
+    private String API_KEY;
+
+    @Value("${weather-api.base.url}")
+    private String BASE_URL;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public ResponseEntity<JSONObject> getDailyWeather(String city){
+        String url = BASE_URL + "/forecast.json" + "?key=" + API_KEY + "&q=" + city + "&days=1&aqi=no&alerts=no";
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject json = (JSONObject) parser.parse(response.getBody());
+            return ResponseEntity.ok().body(json);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ResponseEntity<String> getWeeklyWeather(String city){
+        String url = BASE_URL + "/forecast.json" + "?key=" + API_KEY + "&q=" + city + "&days=7&aqi=no&alerts=no";
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        return ResponseEntity.ok().body(response.getBody());
+    }
+
+    public ResponseEntity<String> getMonthlyWeather(String city){
+        String url = BASE_URL + "/future.json" + "?key=" + API_KEY + "&q=" + city + "&dt=" + LocalDate.now().plusMonths(1);
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        return ResponseEntity.ok().body(response.getBody());
+    }
+}
